bfdf9c7e4d531a24e369408aa8d3cd0aab8503c1
caxaexe <fasvenomo@gmail.com>
2026-02-19 21:14:17 +0200

    proverka

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -43,12 +43,14 @@ class ReportController extends Controller
                 'services.id',
                 'services.name',
                 DB::raw('SUM(booking_service.quantity) as qty'),
-                DB::raw('SUM(booking_service.quantity * booking_service.price) as revenue')
+                DB::raw('SUM(booking_service.quantity * booking_service.price_snapshot) as revenue')
             )
             ->groupBy('services.id', 'services.name')
             ->orderByDesc('revenue')
             ->limit(10)
             ->get();
+
+
 
 
         // 4) Загрузка номеров (кол-во броней и ночей)
