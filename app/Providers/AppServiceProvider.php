272db60af55df8a96437ded25e6fbcafb6fd09c6
caxaexe <fasvenomo@gmail.com>
2026-02-21 22:08:10 +0200

    upd booking

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,32 +1,51 @@
+<?php
+
+namespace App\Providers;
 use App\Models\Booking;
 use Illuminate\Support\Facades\View;
 
-public function boot(): void
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
 {
-    View::composer(['bookings.*'], function ($view) {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-        $today = now()->toDateString();
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        View::composer(['bookings.*'], function ($view) {
 
-        $activeCount = Booking::whereNotIn('status', ['cancelled', 'checked_out'])->count();
+            $today = now()->toDateString();
 
-        $checkInToday = Booking::whereDate('date_from', $today)
-            ->where('status', '!=', 'cancelled')
-            ->count();
+            $activeCount = Booking::whereNotIn('status', ['cancelled', 'checked_out'])->count();
 
-        $checkOutToday = Booking::whereDate('date_to', $today)
-            ->where('status', '!=', 'cancelled')
-            ->count();
+            $checkInToday = Booking::whereDate('date_from', $today)
+                ->where('status', '!=', 'cancelled')
+                ->count();
 
-        $confirmedCount = Booking::where('status', 'confirmed')->count();
+            $checkOutToday = Booking::whereDate('date_to', $today)
+                ->where('status', '!=', 'cancelled')
+                ->count();
 
-        $sumTotal = Booking::sum('total');
+            $confirmedCount = Booking::where('status', 'confirmed')->count();
 
-        $view->with(compact(
-            'activeCount',
-            'checkInToday',
-            'checkOutToday',
-            'confirmedCount',
-            'sumTotal'
-        ));
-    });
+            $sumTotal = Booking::sum('total');
+
+            $view->with(compact(
+                'activeCount',
+                'checkInToday',
+                'checkOutToday',
+                'confirmedCount',
+                'sumTotal'
+            ));
+        });
+    }
 }
