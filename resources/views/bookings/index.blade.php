960f46e27afe97f777d61e36b33b95df02ea05fc
caxaexe <fasvenomo@gmail.com>
2026-06-15 14:00:51 +0300

    update booking view

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -14,7 +14,15 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <x-auth-session-status class="mb-4" :status="session('success')" />
-            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+            @if ($errors->any())
+                <div class="mb-4 p-3 bg-red-100 dark:bg-red-900/30 border border-red-300 dark:border-red-800 text-red-900 dark:text-red-200">
+                    <ul class="list-disc pl-5">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
             <div class="mb-4 flex flex-col sm:flex-row gap-2">
                 <a href="{{ route($prefix.'bookings.create') }}" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
